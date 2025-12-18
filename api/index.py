0cd65bdca52755cdad6ae217621b0e101371d601
0xMichaelRan <michaelran16@gmail.com>
2025-12-18 22:15:21 +0800

    init a fastapi project

---
+++ api/index.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI(title="FastAPI-on-Vercel")
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI + uv + Vercel"}
+
+handler = Mangum(app)      # AWS Lambda handler that Vercel calls
